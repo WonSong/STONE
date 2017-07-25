9bca2fe26fd505c9bae3998bf7ec35dd67e376a5
Won Song <hello@wonsong.me>
2017-07-24 17:04:15 -0700

    Minor code cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -7,48 +7,43 @@ import OneNote from './onenote';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const auth = new Auth();
-    const oneNote = new OneNote();
-    const mdEngine = new MarkdownIt();
+  const auth = new Auth();
+  const oneNote = new OneNote();
+  const mdEngine = new MarkdownIt();
 
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+  let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
 
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-        const selection = editor.selection;
-        const selectedText = editor.document.getText(selection);
-        const rendered = mdEngine.render(selectedText);
+    const selection = editor.selection;
+    const selectedText = editor.document.getText(selection);
+    const rendered = mdEngine.render(selectedText);
 
-        auth
-            .getAuthCode()
-            .then(auth.getAccessToken.bind(auth))
-            .then((token) => {
-                vscode.window.showInformationMessage(token.toString());
+    auth
+      .getAuthCode()
+      .then((code) => auth.getAccessToken(code))
+      .then((token) => {
+        var htmlPayload =
+          "<!DOCTYPE html>" +
+          "<html>" +
+          "<head>" +
+          "    <title>A page created from basic HTML-formatted text (Node.js Sample)</title>" +
+          "    <meta name=\"created\" content=\"" + new Date().toISOString() + "\">" +
+          "</head>" +
+          "<body>" +
+          "    <p>This is a page that just contains some simple <i>formatted</i>" +
+          "    <b>text</b></p>" +
+          "</body>" +
+          "</html>";
+        oneNote.sendToOneNote(token['access_token'], htmlPayload);
+      });
 
+  });
 
-                var htmlPayload =
-                    "<!DOCTYPE html>" +
-                    "<html>" +
-                    "<head>" +
-                    "    <title>A page created from basic HTML-formatted text (Node.js Sample)</title>" +
-                    "    <meta name=\"created\" content=\"" + new Date().toISOString() + "\">" +
-                    "</head>" +
-                    "<body>" +
-                    "    <p>This is a page that just contains some simple <i>formatted</i>" +
-                    "    <b>text</b></p>" +
-                    "</body>" +
-                    "</html>";
-
-                oneNote.sendToOneNote.call(oneNote, token['access_token'], htmlPayload);
-
-            });
-
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
